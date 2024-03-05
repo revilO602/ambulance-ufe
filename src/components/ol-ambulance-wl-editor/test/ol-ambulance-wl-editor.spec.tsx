850ee7aa3a992f2a1f6167e888336ceb2b604f03
Oliver Leontiev <leontiev.oliver@gmail.com>
2024-03-05 10:49:07 +0100

    fix: unit test for editor

--- src/components/ol-ambulance-wl-editor/test/ol-ambulance-wl-editor.spec.tsx
+++ src/components/ol-ambulance-wl-editor/test/ol-ambulance-wl-editor.spec.tsx
@@ -2,17 +2,17 @@ import { newSpecPage } from '@stencil/core/testing';
 import { OlAmbulanceWlEditor } from '../ol-ambulance-wl-editor';
 
 describe('ol-ambulance-wl-editor', () => {
-  it('renders', async () => {
+  it('buttons shall be of different type', async () => {
     const page = await newSpecPage({
       components: [OlAmbulanceWlEditor],
-      html: `<ol-ambulance-wl-editor></ol-ambulance-wl-editor>`,
+      html: `<ol-ambulance-wl-editor entry-id="@new"></ol-ambulance-wl-editor>`,
     });
-    expect(page.root).toEqualHtml(`
-      <ol-ambulance-wl-editor>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </ol-ambulance-wl-editor>
-    `);
+    let items: any = await page.root.shadowRoot.querySelectorAll("md-filled-button");
+    expect(items.length).toEqual(1);
+    items = await page.root.shadowRoot.querySelectorAll("md-outlined-button");
+    expect(items.length).toEqual(1);
+
+    items = await page.root.shadowRoot.querySelectorAll("md-filled-tonal-button");
+    expect(items.length).toEqual(1);
   });
 });
